d749d7732cc9dd9fe973d2b49a5c0992ec2aa362
KeihtOrellana <korellanac@ing.ucsc.cl>
2025-10-23 21:39:14 -0300

    cree la segunda base de datos en el commit anterior, conecte,cree las migraciones y que simulacioncontroller lea esta nueva base de datos, tambien cree los archivos seeders para hacer la carga de datos al sistema, pero igual se puede hacer manual desde xampp

--- database/seeders/SimAlumnoSeeder.php
+++ database/seeders/SimAlumnoSeeder.php
@@ -1,17 +1,17 @@
 <?php
+
 namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB; // ¡Importante!
 
 class SimAlumnoSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     */
     public function run(): void
     {
-        // Especificamos la conexión ANTES de la tabla
-        DB::connection('mysql_simulacion')->table('sim_alumnos')->insert([
-            ['rut' => 11111111, 'nombre' => 'Ana Martínez', 'semestre_inscrito' => '2025-1', 'created_at' => now(), 'updated_at' => now()],
-            ['rut' => 22222222, 'nombre' => 'Bruno Díaz', 'semestre_inscrito' => '2025-1', 'created_at' => now(), 'updated_at' => now()],
-            ['rut' => 1234567, 'nombre' => 'Daniel Soto', 'semestre_inscrito' => '2025-2', 'created_at' => now(), 'updated_at' => now()],
-        ]);
+        //
     }
 }
